9a18a273a7c2c5f47021a0fba7cde1413949e6ae
Maria Bogatyreva <b.maria@cosmos-web.ru>
2026-07-02 12:06:22 +0700

    Правки по замечаниям ревью

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const user = {
         name: 'Maria',
         age: '12',
-        subscription: 'premium',
+        subscription: 'free1',
         lastLogin: '13'
+    }
+
+    const validSubscriptions = ['free', 'basic', 'premium']
+
+    const accessLevelDictionary = {
+        basic: 'Ограниченный доступ',
+        premium: 'Полный доступ',
+        free: 'Доступ только к бесплатному контенту'
     }
 
     // Преобразование типов
@@ -12,20 +20,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const subscription =  String(user.subscription);
     const lastLogin = Number(user.lastLogin);
 
-    // Валидация возраста. Function Expression
-    const validateAge = function(age) {
-        const isNumber = typeof age === 'number' && Number.isFinite(age); // убедились, что перед нами число
-        return isNumber && age > 0; // возврат true, если это число и больше 0
-    }
+    // Валидация возраста. Стрелочная функция
+    const validateAge = (age) =>  typeof age === 'number' && Number.isFinite(age) && age > 0;
 
     // Валидация имени. Стрелочная функция
     const validateName = name =>  name.trim().length !== 0
 
     // Валидация типа подписки. Function Declaration
     function validateSubscription (subscription) {
-        return ( subscription === 'free' ||
-                subscription === 'basic' ||
-                subscription === 'premium' );
+        return validSubscriptions.includes(subscription);
     }
 
     // Валидация времени входа
@@ -34,39 +37,36 @@ document.addEventListener("DOMContentLoaded", () => {
             return null;
         }
 
-        return (lastLogin >=5 && lastLogin <= 11 ) ?
-            'утро' : (lastLogin >=12 && lastLogin <= 17 ) ?
-                'день' : (lastLogin >=18 && lastLogin <= 21 ) ?
-                    'вечер' : 'ночь'
-
+        if (lastLogin >=5 && lastLogin <= 11 ) {
+            return 'утро'
+        } else if (lastLogin >=12 && lastLogin <= 17 ) {
+            return 'день'
+        } else if (lastLogin >=18 && lastLogin <= 21 ) {
+            return 'вечер'
+        } else {
+            return 'ночь'
+        }
     }
 
     // Валидация прав доступа
     function getAccessLevel(subscription) {
-        let accessLevel ;
-        if (subscription === 'basic') {
-            accessLevel = "Ограниченный доступ"
-        } else if (subscription === "premium" ) {
-            accessLevel = "Полный доступ"
-        } else  if (subscription === "free") {
-            accessLevel = "Доступ только к бесплатному контенту"
-        }
-
-        return accessLevel ?? 'доступ не определен'
+        return accessLevelDictionary[subscription]
     }
 
     // Вывод итогового сообщения
     const displayUserInfo = (name, age, lastLogin, subscription) => {
-        const userName = validateName(name) ? name : "Гость",
-              userAge = validateAge(age) ? age : 'не указанное количество',
-              timeOfDay = getTimeOfDay(lastLogin) ?? 'неопределенное время суток',
-              userAccessLevel = validateSubscription(subscription)
+        const userName = validateName(name) ? name : "Гость";
+        const userAge = validateAge(age) ? age : 'не указанное количество';
+        const timeOfDay = getTimeOfDay(lastLogin) ?? 'неопределенное время суток';
+        const userAccessLevel = validateSubscription(subscription)
                 ? getAccessLevel(subscription)
                 : "Доступ не определен";
 
         let msg = `Привет, ${userName}! Вам ${userAge} лет. Сейчас ${timeOfDay}. Уровень доступа: ${userAccessLevel}.`
 
-        userAge < 18 ? msg += 'Вы несовершеннолетний пользователь' : '';
+        if (userAge < 18) {
+            msg += 'Вы несовершеннолетний пользователь'
+        }
 
         const lastEntryMsg = "Позднее время для входа."
         if (lastLogin >=22 || lastLogin <5) {
